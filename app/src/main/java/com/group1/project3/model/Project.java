178a369318f94a53ddf9a8e8f944db4dbac151d1
surajsherchan <surajsherchan@hotmail.com>
2022-04-14 12:50:29 -0700

    project

---
+++ app/src/main/java/com/group1/project3/model/Project.java
@@ -0,0 +1,16 @@
+package com.group1.project3.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.group1.project3.R;
+
+public class Project extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project);
+    }
+}
