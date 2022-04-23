48c56691e3bbe0e0799063d8719bee984595855d
Victor Pan <41593388+LordArugula@users.noreply.github.com>
2022-04-22 17:14:20 -0700

    Update RegisterActivity

--- app/src/main/java/com/group1/project3/RegisterActivity.java
+++ app/src/main/java/com/group1/project3/RegisterActivity.java
@@ -1,112 +1,121 @@
 package com.group1.project3;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.group1.project3.model.User;
+import com.group1.project3.repository.FirestoreUserRepository;
+import com.group1.project3.repository.UserRepository;
 import com.group1.project3.util.FirebaseUtil;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText text_firstName;
-    private EditText text_lastName;
-    private EditText text_username;
-    private EditText text_email;
-    private EditText text_password;
+    private EditText input_firstName;
+    private EditText input_lastName;
+    private EditText input_username;
+    private EditText input_email;
+    private EditText input_password;
+
+    private UserRepository userRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        text_firstName = findViewById(R.id.register_text_firstName);
-        text_lastName = findViewById(R.id.register_text_lastName);
-        text_username = findViewById(R.id.register_text_username);
-        text_email = findViewById(R.id.register_text_emailAddress);
-        text_password = findViewById(R.id.register_text_password);
-        EditText confirmPasswordText = findViewById(R.id.register_text_confirmPassword);
+        userRepository = new FirestoreUserRepository();
 
-        Button registerButton = findViewById(R.id.register_submit_button);
-        registerButton.setEnabled(false);
+        input_firstName = findViewById(R.id.register_input_firstName);
+        input_lastName = findViewById(R.id.register_input_lastName);
+        input_username = findViewById(R.id.register_input_username);
+        input_email = findViewById(R.id.register_input_email);
+        input_password = findViewById(R.id.register_input_password);
+        EditText input_confirmPassword = findViewById(R.id.register_input_confirm_password);
 
-        TextWatcher textWatcher = new RegisterFormValidator(text_firstName, text_lastName, text_username, text_email, text_password, confirmPasswordText, registerButton);
-        text_firstName.addTextChangedListener(textWatcher);
-        text_lastName.addTextChangedListener(textWatcher);
-        text_username.addTextChangedListener(textWatcher);
-        text_email.addTextChangedListener(textWatcher);
-        text_password.addTextChangedListener(textWatcher);
-        confirmPasswordText.addTextChangedListener(textWatcher);
+        Button button_signUp = findViewById(R.id.register_button_signUp);
+        button_signUp.setOnClickListener(this::onClickSignUpButton);
+        button_signUp.setEnabled(false);
+
+        TextWatcher textWatcher = new RegisterFormValidator(input_firstName, input_lastName, input_username, input_email, input_password, input_confirmPassword, button_signUp);
+        input_firstName.addTextChangedListener(textWatcher);
+        input_lastName.addTextChangedListener(textWatcher);
+        input_username.addTextChangedListener(textWatcher);
+        input_email.addTextChangedListener(textWatcher);
+        input_password.addTextChangedListener(textWatcher);
+        input_confirmPassword.addTextChangedListener(textWatcher);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.register_title);
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
-    public void toMainMenu(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
-    public void registerAccount(View view) {
-        String email = text_email.getText().toString();
-        String password = text_password.getText().toString();
-
-        FirebaseAuth auth = FirebaseUtil.getAuth();
-        auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser firebaseUser = auth.getCurrentUser();
-                            assert firebaseUser != null;
-
-                            String firstName = text_firstName.getText().toString().trim();
-                            String lastName = text_lastName.getText().toString().trim();
-                            String username = text_username.getText().toString().trim();
-
-                            User user = new User(firebaseUser.getUid(), firstName, lastName, username, email);
-
-                            UserProfileChangeRequest.Builder changeRequestBuilder = new UserProfileChangeRequest.Builder()
-                                    .setDisplayName(username);
-                            firebaseUser.updateProfile(changeRequestBuilder.build());
-
-                            FirebaseUtil.getFirestore().collection("users")
-                                    .document(firebaseUser.getUid())
-                                    .set(user.serializeAsMap())
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()) {
-                                                Log.d("REGISTER", "Registered account");
-                                            }
-                                        }
-                                    });
-
-                            launchProfileMenuActivity(firebaseUser);
-                        } else {
-                            Toast.makeText(RegisterActivity.this, "Failed to register account.", Toast.LENGTH_SHORT)
-                                    .show();
-                        }
-                    }
+    /**
+     * OnClick callback for the sign up button.
+     * Signs the user up.
+     *
+     * @param view the button that was clicked on.
+     */
+    private void onClickSignUpButton(View view) {
+        registerAccount()
+                .addOnSuccessListener(unused -> {
+                    Log.d("Register", "Registered Account");
+                    finish();
+                })
+                .addOnFailureListener(exception -> {
+                    Log.e("Register", exception.getMessage());
+                    Toast.makeText(RegisterActivity.this, exception.getMessage(), Toast.LENGTH_SHORT)
+                            .show();
                 });
     }
 
-    private void launchProfileMenuActivity(FirebaseUser user) {
-        Intent intent = new Intent(this, ProjectMenuActivity.class);
-        startActivity(intent);
+    /**
+     * Registers the user's account.
+     *
+     * @return a {@link Task<Void>} representing the register request.
+     */
+    private Task<Void> registerAccount() {
+        String email = input_email.getText().toString().trim();
+        String password = input_password.getText().toString();
+
+        FirebaseAuth auth = FirebaseUtil.getAuth();
+        return auth.createUserWithEmailAndPassword(email, password)
+                .addOnFailureListener(exception -> Toast.makeText(RegisterActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show())
+                .onSuccessTask((AuthResult authResult) -> {
+                    FirebaseUser firebaseUser = authResult.getUser();
+                    assert firebaseUser != null;
+
+                    String firstName = input_firstName.getText().toString().trim();
+                    String lastName = input_lastName.getText().toString().trim();
+                    String username = input_username.getText().toString().trim();
+                    Task<Void> updateProfileTask = FirebaseUtil.updateProfile(username, null);
+
+                    User user = new User(firebaseUser.getUid(), firstName, lastName, username, email);
+                    Task<Void> createUserTask = userRepository.createUser(user);
+                    return Tasks.whenAll(updateProfileTask, createUserTask);
+                });
     }
 }
